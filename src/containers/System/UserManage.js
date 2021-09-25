d163e1e927f5f41b93c3c7f7da649d047e7acedf
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-09-25 11:57:25 +0700

    #6 ViewAllUsers

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import {getAllUsers} from '../../services/userService';
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode === 0){
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+        console.log('get users from node: ', response)
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center">
+                    Manage Users
+                </div>
+                <div className="users-table mt-3 mx-2">
+                <table id="customers">
+                    <tr>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Address</th>
+                        <th>Phone Number</th>
+                        <th>Action</th>
+                    </tr>
+                        { arrUsers && arrUsers.map((item, index) => {
+                            console.log('check map: ', item, index)
+                            return(
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>{item.phonenumber}</td>
+                                    <td>
+                                        <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
+                                        <button className="btn-delete"><i className="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+                </table>
+                </div>
+            </div>
         );
     }
 
