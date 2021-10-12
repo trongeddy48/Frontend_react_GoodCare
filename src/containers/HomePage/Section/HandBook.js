1d0d25d3a6c095601742613e32e37151bd7d9bd0
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-10-12 11:02:35 +0700

    #16 AllSlider

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,43 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Specialty.scss';
 import { FormattedMessage } from 'react-intl';
 import Slider from 'react-slick';
 
-class Specialty extends Component {
+class HandBook extends Component {
 
     render() {
         return (
-            <div className="section-share section-specialty">
+            <div className="section-share section-handbook">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
+                        <span className="title-section">Cẩm nang</span>
                         <button className="btn-section">Xem thêm</button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
                             <div className="section-custom">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ xương, khớp 1</div>
                             </div>
                             <div className="section-custom">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ xương, khớp 2</div>
                             </div>
                             <div className="section-custom">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ xương, khớp 3</div>
                             </div>
                             <div className="section-custom">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ xương, khớp 4</div>
                             </div>
                             <div className="section-custom">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ xương, khớp 5</div>
                             </div>
                             <div className="section-custom">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ xương, khớp 6</div>
                             </div>
                         </Slider>
@@ -60,4 +59,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
---
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import Slider from 'react-slick';
+
+class OutStandingDoctor extends Component {
+
+    render() {
+        
+        return (
+            <div className="section-share section-outstanding-doctor">
+                <div className="section-container">
+                    <div className="section-header">
+                        <span className="title-section">Bác sĩ nổi bật</span>
+                        <button className="btn-section">Xem thêm</button>
+                    </div>
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+                            <div className="section-custom">
+                                <div className="custom-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, tiến sĩ Trọng Nguyễn</div>
+                                        <div>Cơ xương, khớp 1</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-custom">
+                                <div className="custom-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, tiến sĩ Trọng Nguyễn</div>
+                                        <div>Cơ xương, khớp 2</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-custom">
+                                <div className="custom-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, tiến sĩ Trọng Nguyễn</div>
+                                        <div>Cơ xương, khớp 3</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-custom">
+                                <div className="custom-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, tiến sĩ Trọng Nguyễn</div>
+                                        <div>Cơ xương, khớp 4</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-custom">
+                                <div className="custom-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, tiến sĩ Trọng Nguyễn</div>
+                                        <div>Cơ xương, khớp 5</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-custom">
+                                <div className="custom-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, tiến sĩ Trọng Nguyễn</div>
+                                        <div>Cơ xương, khớp 6</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
