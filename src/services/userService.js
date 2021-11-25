7e23c4e12dc5f94eec637aaa7d7d49def4572a1f
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-11-25 14:33:37 +0700

    #58 Clinic

--- src/services/userService.js
+++ src/services/userService.js
@@ -82,6 +82,10 @@ const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
 
+const createNewClinic = (data) => {
+    return axios.post('/api/create-new-clinic', data)
+}
+
 export { 
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUserService, 
@@ -91,5 +95,6 @@ export {
     getScheduleDoctorByDate, getExtraInfoDoctorById,
     getProfileDoctorById, postPatientBookAppointment,
     postVerifyBookAppointment, createNewSpecialty,
-    getAllSpecialty, getAllDetailSpecialtyById
+    getAllSpecialty, getAllDetailSpecialtyById,
+    createNewClinic, 
 }
